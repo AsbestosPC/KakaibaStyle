8afe126e54b52559868efc726e7f9bc2c4d2a534
AsbestosPC <114710367+AsbestosPC@users.noreply.github.com>
2022-10-10 20:38:57 -0700

    Dark mode affects header

--- main.js
+++ main.js
@@ -4,11 +4,19 @@ function changeDarkMode() {
 	darkMode = !darkMode
 
 	if (darkMode) {
-		document.body.classList.remove("light")
-		document.body.classList.add("dark")
+		// document.body.classList.remove("light")
+		// document.body.classList.add("dark")
+
+		// document.getElementById("header").classList.remove("light");
+		// document.getElementById("header").classList.add("dark");]
+
+		document.querySelectorAll('.light').forEach(e => e.classList.replace('light', 'dark'))
 	}
 	else {
 		document.body.classList.remove("dark")
 		document.body.classList.add("light")
+
+		document.getElementById("header").classList.remove("dark");
+		document.getElementById("header").classList.add("light");
 	}
 }
