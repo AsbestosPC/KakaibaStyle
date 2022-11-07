5cd263d80cccd7489cd89a24c4cd8974ad3255e6
AsbestosPC <114710367+AsbestosPC@users.noreply.github.com>
2022-11-06 19:23:51 -0800

    Remove commented code, added comments

--- main.js
+++ main.js
@@ -1,32 +1,15 @@
 let darkModeEnbaled = false
 
+// replace all instances of class "light" with "dark"
 function darkMode() {
-	// let elements = document.getElementsByClassName("light")
-
-	// for(var i = 0; i < elements.length; i++)
-	// {
-	// 	console.log(i);
-    // 	elements[i].classList.add('dark');
-    // 	elements[i].classList.remove('light');
-	// }
-
 	let elements = document.querySelectorAll('.light')
 	elements.forEach( x => {
 		x.classList.add('dark');
 		x.classList.remove('light');
 	})
 }
-
+// replace all instances of class "dark" with "light"
 function lightMode() {
-	// let elements = document.getElementsByClassName("dark")
-
-	// for(var i = 0; i < elements.length; i++)
-	// {
-	// 	console.log(i)
-    // 	elements[i].classList.add('light');
-    // 	elements[i].classList.remove('dark');
-	// }
-
 	let elements = document.querySelectorAll('.dark')
 	elements.forEach( x => {
 		x.classList.add('light');
@@ -37,14 +20,6 @@ function lightMode() {
 function changeDarkMode() {
 	darkModeEnbaled = !darkModeEnbaled
 
-	if (darkModeEnbaled) {
-		// document.body.classList.remove("light")
-		// document.body.classList.add("dark")
-
-		// document.getElementById("header").classList.remove("light");
-		// document.getElementById("header").classList.add("dark");]
-
-		// document.querySelectorAll('.light').forEach(e => e.classList.replace('light', 'dark'))
 		darkMode();
 		console.log("Changed to dark")
 	}
